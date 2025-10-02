2f6e362d4d663a020943c9aa00fa17c4fbb30883
YLudo <ludovic.roux31@ynov.com>
2025-10-02 15:42:21 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -10,42 +10,12 @@ export async function middleware(request: NextRequest) {
     }
 
     const accessToken = request.cookies.get("access_token")
-    console.log(accessToken);
 
     if (!accessToken) {
         return NextResponse.redirect(new URL("/login", request.url))
     }
 
-    try {
-        const response = await fetch(process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT!, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Cookie: request.headers.get("cookie") || "",
-            },
-            credentials: "include",
-            body: JSON.stringify({
-                query: `
-                mutation RefreshToken {
-                    refreshToken {
-                    __typename
-                    }
-                }
-                `,
-            }),
-        })
-
-        const result = await response.json()
-
-        if (result.errors) {
-            return NextResponse.redirect(new URL("/login", request.url))
-        }
-
-        return NextResponse.next()
-    } catch (error) {
-        console.error("Middleware token refresh error:", error)
-        return NextResponse.next()
-    }
+    return NextResponse.next();
 }
 
 export const config = {
